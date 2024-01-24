28dc0115e59356a8e6e3e9616901c64f84401a9c
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-01-24 22:13:06 +0530

    added anathor files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import './App.css';
-import Home from './components/Home';
-import About from './components/about/about';
-import Explore from './components/about/explore';
-import Lists from './components/about/lists';
-import Navbar from './components/navbar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/about/about";
+import Explore from "./components/about/explore";
+import Lists from "./components/about/lists";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <About />
-      <Lists />
-      <Explore />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="/" Component={Home}></Route>
+      </Routes>
+      <div className="App">
+        <Navbar />
+        <Home />
+        <About />
+        <Lists />
+        <Explore />
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer>
+        <p>&copy; 2023 Your Website. All rights reserved.</p>
+        <p>
+            {/* Social media links */}
+            <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer">Facebook</a>
+            <a href="https://twitter.com/yourhandle" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://www.instagram.com/youraccount" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
